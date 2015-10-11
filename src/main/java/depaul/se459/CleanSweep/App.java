03e57655840d61da9c78af0d7dd2b755ae8c95cd
Tu Vo <tuvo@Tus-MacBook-Pro.local>
2015-10-10 21:15:46 -0400

    initial push

---
+++ src/main/java/depaul/se459/CleanSweep/App.java
@@ -0,0 +1,13 @@
+package depaul.se459.CleanSweep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
